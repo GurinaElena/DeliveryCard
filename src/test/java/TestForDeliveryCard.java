b0d59f385addd44ebb4635fd0f1e53b44175de85
Gurina Elena <gurina_elena@inbox.ru>
2020-06-13 22:39:03 +0300

    Добавление основных файлов

---
+++ src/test/java/TestForDeliveryCard.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestForDeliveryCard {
+
+    @Test
+    void shouldDeliveryCard (){
+
+        Calendar c = new GregorianCalendar();
+        c.add(Calendar.DAY_OF_YEAR, 3);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+        String str = format1.format(c.getTime());
+
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Ярославль");
+        $("[data-test-id=date] input").setValue(str);
+        $("[data-test-id=name] input").setValue("Гурина Елена");
+        $("[data-test-id=phone] input").setValue("+79519638383");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(((exactText("Забронировать")))).click();
+        $(withText("Успешно!")).waitUntil(visible, 10000);
+
+    }
+}
