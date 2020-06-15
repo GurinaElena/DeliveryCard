68664390820baed0389b76e64863ca9ac6d1a9ac
Gurina Elena <gurina_elena@inbox.ru>
2020-06-15 22:39:51 +0300

    Изменения в тесте

--- src/test/java/TestForDeliveryCard.java
+++ src/test/java/TestForDeliveryCard.java
@@ -1,5 +1,7 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -18,18 +20,21 @@ public class TestForDeliveryCard {
     void shouldDeliveryCard (){
 
         Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 3);
+        c.add(Calendar.DAY_OF_YEAR, 5);
         SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
         String str = format1.format(c.getTime());
 
+        String deleteDate = Keys.chord(Keys.CONTROL, "a") + Keys.DELETE;
+
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Ярославль");
+        $("[data-test-id=date] input").setValue(deleteDate);
         $("[data-test-id=date] input").setValue(str);
         $("[data-test-id=name] input").setValue("Гурина Елена");
         $("[data-test-id=phone] input").setValue("+79519638383");
         $("[data-test-id=agreement]").click();
-        $$("button").find(((exactText("Забронировать")))).click();
-        $(withText("Успешно!")).waitUntil(visible, 10000);
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).waitUntil(visible, 15000);
 
     }
 }
